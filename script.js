3a7409f65b62800a0a36e55b2204d0e32291b1a4
Valencius Apriady <v.apriady@gmail.com>
2024-11-03 23:24:34 +0700

    feat: integration with BE code

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 function calculateSurface() {
   const length = document.getElementById('surfaceInput').value;
-  document.getElementById('surfaceResult').innerText =
-    'Hasil luas persegi: ' + length;
+  fetch('http://52.23.185.228:8080/function/javascript-fn', {
+    body: JSON.stringify({ panjang: length }),
+    method: 'POST',
+  })
+    .then((resp) => resp.json())
+    .then((data) => {
+      const area = data.luas;
+      document.getElementById('surfaceResult').innerText =
+        'Hasil luas persegi: ' + area;
+    });
 }
 
 function calculateSurfaceArea() {
   const length = document.getElementById('surfaceAreaInput').value;
-  document.getElementById('surfaceAreaResult').innerText =
-    'Hasil luas permukaan kubus: ' + length;
+
+  fetch('http://34.235.139.199:8080/function/fn-luas-permukaan-kubus', {
+    body: JSON.stringify({ sisi: length }),
+    method: 'POST',
+  })
+    .then((resp) => resp.json())
+    .then((data) => {
+      const area = data.surfaceArea;
+      document.getElementById('surfaceResult').innerText =
+        'Hasil luas permukaan kubus: ' + area;
+    });
 }
