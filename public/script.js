2beef8d19b851644c69112ad060c078ef06b3b14
Valencius Apriady <v.apriady@gmail.com>
2024-11-04 13:21:13 +0700

    refactor: change backend API url

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
 function calculateSurface() {
   const length = document.getElementById('surfaceInput').value;
-  fetch('http://52.23.239.207:8080/function/luas-persegi', {
+  fetch('http://54.172.163.137:8080/function/luas-persegi', {
     body: JSON.stringify({ panjang: length }),
     method: 'POST',
   })
@@ -15,14 +15,14 @@ function calculateSurface() {
 function calculateSurfaceArea() {
   const length = document.getElementById('surfaceAreaInput').value;
 
-  fetch('http://34.235.139.199:8080/function/fn-luas-permukaan-kubus', {
+  fetch('http://184.73.66.5:8080/function/fn-luas-permukaan-kubus', {
     body: JSON.stringify({ sisi: length }),
     method: 'POST',
   })
     .then((resp) => resp.json())
     .then((data) => {
-      const area = data.surfaceArea;
-      document.getElementById('surfaceResult').innerText =
+      const area = data.surface_area;
+      document.getElementById('surfaceAreaResult').innerText =
         'Hasil luas permukaan kubus: ' + area;
     });
 }
